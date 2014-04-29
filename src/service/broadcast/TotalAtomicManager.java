1658d95b28de60764d56547e4d3586d14c4eb5c9
Baylac-Jacqué Félix <baylac.felix@gmail.com>
2014-04-29 21:00:10 +0200

    Fixing empty token issue.

--- src/service/broadcast/TotalAtomicManager.java
+++ src/service/broadcast/TotalAtomicManager.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package service.broadcast;
 
 import communication.CommunicationException;
@@ -19,7 +18,8 @@ import service.IIdentification;
  *
  * @author ninjatrappeur
  */
-public class TotalAtomicManager extends Thread{
+public class TotalAtomicManager extends Thread {
+
     private final SynchronizedBuffer<TotalAtomicMessage> _ackBuffer;
     private final SynchronizedBuffer<TotalAtomicMessage> _tokenBuffer;
     private final SynchronizedBuffer<TotalAtomicMessage> _inputBuffer;
@@ -29,8 +29,8 @@ public class TotalAtomicManager extends Thread{
     private final ReliableBroadcastService _reliableService;
     private boolean _isOn;
     private final TotalAtomicBroadcastService _broadcastService;
-    
-    public TotalAtomicManager (
+
+    public TotalAtomicManager(
             SynchronizedBuffer<TotalAtomicMessage> ack,
             SynchronizedBuffer<TotalAtomicMessage> input,
             SynchronizedBuffer<Message> output,
@@ -47,84 +47,93 @@ public class TotalAtomicManager extends Thread{
         _isOn = true;
         _broadcastService = bs;
     }
-    
+
     @Override
     public void run() {
-        while(_isOn) {
+        while (_isOn) {
             TotalAtomicMessage message = fetchMessage();
-            switch(message.getType()) {
+            switch (message.getType()) {
                 //Si on reçoit du payload, on envoie un acquittement et on fait passer dans le buffer sortie.
-                case PAYLOAD :
+                case PAYLOAD:
                     try {
                         System.out.println("ACK: Message reçu, on envoie un ACK.");
                         _reliableService.broadcast(new TotalAtomicMessage(_idServ.getMyIdentifier(),
-                            message.getProcessIdSender(), "ACK", TotalAtomicType.ACK));
-                    } catch(CommunicationException c) {System.err.println("Impossible d'envoyer un message TotalAtomicManager.run " + c);}
+                                message.getProcessIdSender(), "ACK", TotalAtomicType.ACK));
+                    } catch (CommunicationException c) {
+                        System.err.println("Impossible d'envoyer un message TotalAtomicManager.run " + c);
+                    }
                     _outputBuffer.addElement(new Message(message.getProcessIdSender(), message.getData()));
                     break;
-                    
+
                 //Si on reçoit le jeton, on le recopie et on réveille le service qui était en attente à l'aide du buffer de jeton.    
-                case TOKEN :
-                    System.out.println("TOKEN: token reçu de "+ message.getProcessIdSender());
-                    if(message.getProcessIdReceiver().equals(_idServ.getMyIdentifier())) {
+                case TOKEN:
+                    System.out.println("TOKEN: token reçu de " + message.getProcessIdSender());
+                    if (message.getProcessIdReceiver().equals(_idServ.getMyIdentifier())) {
                         _tokenBuffer.addElement(message);
                     }
                     break;
-                    
+
                 //Si on reçoit une requête de jeton, on regarde si on a le jeton et que l'on ne s'en sert pas.
                 //Si c'est le cas on l'envoie. Sinon on incrémente les demandes.
                 case TOKEN_REQUEST:
                     System.out.println("TOKEN: Token request reçu de " + message.getProcessId());
-                    if(_request.containsKey(message.getProcessIdSender())) {
+                    if (_request.containsKey(message.getProcessIdSender())) {
                         _request.put(message.getProcessIdSender(), _request.get(message.getProcessIdSender()) + 1);
                     } else {
                         _request.put(message.getProcessIdSender(), 1);
                     }
-                    if(_broadcastService.getTokenState() && !_broadcastService.getTokenUsage()) {
-                        try{
+                    if (_broadcastService.getTokenState() && !_broadcastService.getTokenUsage()) {
+                        try {
+                            HashMap<ProcessIdentifier, Integer> token;
+                            token = _broadcastService.getToken();
+                            ProcessIdentifier myId = _idServ.getMyIdentifier();
                             System.out.println("TOKEN: On envoie le token à " + message.getProcessIdSender());
-                            _reliableService.broadcast(new TotalAtomicMessage(_idServ.getMyIdentifier(), 
+                            if (token.containsKey(myId)) {
+                                token.put(myId, token.get(myId) + 1);
+                            } else {
+                                token.put(myId, 1);
+                            }
+                            _reliableService.broadcast(new TotalAtomicMessage(_idServ.getMyIdentifier(),
                                     message.getProcessIdSender(), _broadcastService.getToken(),
                                     TotalAtomicType.TOKEN));
                             _broadcastService.setTokenState(false);
+                        } catch (CommunicationException c) {
+                            System.err.println("Impossible d'envoyer un message TotalAtomicManager.run " + c);
                         }
-                        catch(CommunicationException c) {System.err.println("Impossible d'envoyer un message TotalAtomicManager.run " + c);}
                     } else {
                         _request.put(message.getProcessIdSender(), _request.get(message.getProcessIdSender()) + 1);
                     }
                     break;
-                    
+
                 //On fait passer l'acquittement au service (c'est lui qui se charge de les compter).    
                 case ACK:
-                    System.out.println("ACK: ACK reçu de "+ message.getProcessIdSender());
-                    if(message.getProcessIdReceiver().getId() == _idServ.getMyIdentifier().getId())
+                    System.out.println("ACK: ACK reçu de " + message.getProcessIdSender());
+                    if (message.getProcessIdReceiver().getId() == _idServ.getMyIdentifier().getId()) {
                         _ackBuffer.addElement(message);
+                    }
                     break;
-                    
+
                 default:
                     throw new IllegalStateException("TotalAtomicManager.run: message type unknown: " + message.getType().toString());
             }
         }
     }
-    
-    
-    public TotalAtomicMessage fetchMessage()
-    {
+
+    public TotalAtomicMessage fetchMessage() {
         Message mess = _inputBuffer.removeElement(true);
-           
-        if(! (mess instanceof TotalAtomicMessage) )
-        {
-            throw new IllegalStateException("TotalAtomicManager.fetchMessage: messages in total mode should be of type TotalAtomicMessage.\n\t found "+mess.getClass().getName());
+
+        if (!(mess instanceof TotalAtomicMessage)) {
+            throw new IllegalStateException("TotalAtomicManager.fetchMessage: messages in total mode should be of type TotalAtomicMessage.\n\t found " + mess.getClass().getName());
         }
-           
+
         return (TotalAtomicMessage) mess;
-        }
-    
+    }
+
     public void setIdentificationService(IIdentification serv) {
         _idServ = serv;
     }
-    
-    public void terminate () {
+
+    public void terminate() {
         _isOn = false;
     }
 }
